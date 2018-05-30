30b86baec41c5f47bca060a64f26178271ad0a5c
icl <776172520@qq.com>
2018-05-30 23:17:45 +0800

    添加菜单实体

---
+++ capacity-system/src/main/java/com/capacity/platform/system/entity/SysMenu.java
@@ -0,0 +1,12 @@
+package com.capacity.platform.system.entity;
+
+import com.capacity.platform.common.entity.BaseEntity;
+
+/**
+ * Author: icl
+ * Date:2018/05/30
+ * Description: 菜单
+ * Created by icl on 2018/05/30.
+ */
+public class SysMenu extends BaseEntity {
+}
