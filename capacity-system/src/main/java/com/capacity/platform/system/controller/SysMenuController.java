6f6b439b89884891dbaf649a7294d5a31153b2f3
icl <776172520@qq.com>
2018-05-30 23:19:38 +0800

    菜单controller

---
+++ capacity-system/src/main/java/com/capacity/platform/system/controller/SysMenuController.java
@@ -0,0 +1,10 @@
+package com.capacity.platform.system.controller;
+
+/**
+ * Author: icl
+ * Date:2018/05/30
+ * Description:
+ * Created by icl on 2018/05/30.
+ */
+public class SysMenuController {
+}
